b05b097f3a0a5fb78d9ee7df9da554bad96299d8
Nathan R. Liu <nathanliu17@gmail.com>
2021-12-04 22:07:39 -0800

    add hexagon class

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class HexNode extends React.Component {
+	render(){
+		return (
+			<div className="hex outerHex">
+				<div className="hex innerHex">
+					<p className="nodeText">{this.props.text}</p>
+				</div>
+			</div>
+		)
+	}
+}
+var nodeList = []
+
+var page = []
+nodeList.push(<HexNode text="1"/>)
+function connectNodes(){
+	return(<svg class="connection" width="500" height="500"><line x1="50" y1="50" x2="350" y2="350" stroke="white"/></svg>)
+}
+page.push(connectNodes());
+
+page.push(nodeList);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	page,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
